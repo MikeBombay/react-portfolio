3fe50baaa6e8eb7f045b5a25fba6eb4d3f9bc07b
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-10-11 18:57:06 -0400

    components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div className="wrapper">
+            {/* <NavBar /> */}
+            <Projects />
+            <About />
+            <Resume />
+            <Contact />
+          </div>
+      </div>
   );
 }
 
 export default App;
+
+
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section>
+      <h1 id="contact">Contact me 613-323-1129</h1>
+    </section>
+  );
+}
+
+export default Contact;
