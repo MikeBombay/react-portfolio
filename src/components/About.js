9c0c2b195dd423beba8f228351586ef6de74fba5
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-10-22 19:25:47 -0400

    ??

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ function About() {
     This is a paragraph about me. This paragraph tells the reader things things things about me. This is a paragraph about me. This paragraph tells the reader things about me. This is a paragraph about me. This paragraph tells the reader things about me.
     </div>
     <div class="col">
-    <img src={require(`./assets/head.png`)}/>
+    <img src={require(`./assets/head.png`).default}/>
     </div>
     </div>
     </div>
