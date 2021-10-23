ac64c3f85fae28b5db9022539f1f25cfd9217bea
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-10-22 20:36:00 -0400

    requirements mostly met

--- src/components/About.js
+++ src/components/About.js
@@ -5,17 +5,20 @@ function About() {
     <section class="about">
       <h2>About Me</h2>
       <div class="container">
-  <div class="row">
-    <div class="col">
-    This is a paragraph about me. This paragraph tells the reader things things things about me. This is a paragraph about me. This paragraph tells the reader things about me. This is a paragraph about me. This paragraph tells the reader things about me.
-    </div>
-    <div class="col">
-    <img src={require(`./assets/head.png`).default}/>
-    </div>
-    </div>
+      <ul className="flex-row mobile-row">
+<li className="padding">
+This is not going to be my real portfolio as I am not comfortable enough with React to use this as a pofrtfolio. This is a paragraph about me. This paragraph tells the reader things things things about me. This is a paragraph about me. This paragraph tells the reader things about me. This is a paragraph about me. This paragraph tells the reader things about me.
+</li>
+<li className="padding">
+<img src={require(`./assets/head.png`).default}/>
+</li>
+</ul>
+ 
     </div>
     </section>
   );
 }
+
+
 
 export default About;
